fac6329469a94b79db9a6408a271fddf6469fc5a
Андрей Романов <human1488pwnz@yandex.ru>
2024-10-06 19:36:34 +0300

    feat: bullet component

---
+++ components/bullet.tsx
@@ -0,0 +1,9 @@
+import { IoArrowForwardOutline } from "react-icons/io5"
+
+export const Bullet = () => {
+    return (
+        <div className="size-6 flex justify-center items-center text-white rounded-full bg-orange-special">
+            <IoArrowForwardOutline className="-rotate-45" />
+        </div>
+    )
+}
