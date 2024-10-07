ef7d9994ca40ad8017990615aca71744519bc179
Андрей Романов <human1488pwnz@yandex.ru>
2024-10-07 16:10:54 +0300

    Add line-height & padding-y

--- components/hero-block/hero.tsx
+++ components/hero-block/hero.tsx
@@ -17,14 +17,14 @@ const toggleProps = [
 
 export const Hero = () => {
     return (
-        <div className="px-80">
+        <div className="px-80 py-28">
             <div className="space-y-8 text-black-special">
                 <h1 className="font-bold text-[80px] leading-none">Монетизируйте клиентскую<br />базу, не снижая NPS</h1>
                 <h2 className="text-2xl">Найдите идеальный баланс выручки<br />и удовлетворённости пользователей с платформой<br />рекламной монетизации</h2>
             </div>
             <Button variant='orange' className="mt-20 px-11 py-3 rounded-lg">Заказать звонок</Button>
             <div className="flex justify-between mt-[180px]">
-                <h1 className="font-bold text-[32px] leading-none">Дополнительные источники<br />выручки для разных<br />компаний</h1>
+                <h1 className="font-bold text-[32px] leading-10">Дополнительные источники<br />выручки для разных<br />компаний</h1>
                 <Toggle props={toggleProps} />
             </div>
         </div>
