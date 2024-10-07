ee5d760efba0af1b2c86206e296f97f08bf2f61b
Андрей Романов <human1488pwnz@yandex.ru>
2024-10-07 13:39:14 +0300

    add position tracker

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,10 +1,37 @@
+'use client'
+
 import Link from "next/link"
 
 import { Menu } from "./menu"
+import { cn } from "@/lib/utils"
+import { useEffect, useRef, useState } from "react"
 
 export const Header = () => {
+    const [hasShadow, setHasShadow] = useState(false)
+
+    const headerRef = useRef<HTMLElement>(null)
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (headerRef.current) {
+                const { top } = headerRef.current.getBoundingClientRect()
+
+                setHasShadow(top === 0)
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll)
+        handleScroll()
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
+
     return (
-        <header className="flex justify-between items-center sticky top-0 px-80 py-4 bg-gray-03 shadow-special">
+        <header ref={headerRef} className={cn("flex justify-between items-center sticky top-0 px-80 py-4 bg-gray-03 transition-shadow z-50",
+            hasShadow && 'shadow-special'
+        )}>
             <Link href='/' className="px-12 py-1 text-center font-black text-2xl text-black-special bg-white rounded-lg">LOGO</Link>
             <Menu />
         </header>
